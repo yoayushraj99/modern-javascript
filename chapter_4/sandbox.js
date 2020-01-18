2bde62b18774810944f4d549ccb6a2d415ac1d39
yoayushraj99 <56199773+yoayushraj99@users.noreply.github.com>
2020-01-18 15:44:58 +0530

    Update sandbox.js

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -2,7 +2,8 @@
 function greet(){
   console.log('hello there');
 }
-
+// Difference b/w function declaration and function expresion is that in function expression we equate a variable with a function /string /no.
+//and we have to put a semi-colon in the end of function expression.
 // function expression
 const speak = function(){
   console.log('good day!');
