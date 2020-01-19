6a779fdcc121618797a743dd490bc684a114bc5a
yoayushraj99 <56199773+yoayushraj99@users.noreply.github.com>
2020-01-19 17:16:10 +0530

    Update sandbox.js

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -8,7 +8,7 @@ const greet = () => {
 
 let resultOne = greet();
 console.log(resultOne);
-
+// a method is a function that belongs to a class. In JavaScript, however, a method is a function that belongs to an object.
 // method
 
 let resultTwo = name.toUpperCase();
