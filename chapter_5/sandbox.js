f39fc70f9579e925ce213442252d85fb918351e3
yoayushraj99 <56199773+yoayushraj99@users.noreply.github.com>
2020-01-22 19:04:41 +0530

    Update sandbox.js

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -21,6 +21,18 @@ let user = {
     })
   }
 };
+//The JavaScript this keyword refers to the object it belongs to.
+/*It has different values depending on where it is used:
 
+In a method, this refers to the owner object.
+Alone, this refers to the global object.
+In a function, this refers to the global object.
+In a function, in strict mode, this is undefined.
+In an event, this refers to the element that received the event.
+Methods like call(), and apply() can refer this to any object.*/
+//one more thing we cannot use arrow function if we use this method it only works with usual function.
+//short hand of usual function.
+//eg:logout:function(){ }(long form of usual function).
+//eg:logout(){}(shorthand of usual function).
 // console.log(this);
 user.logBlogs();
