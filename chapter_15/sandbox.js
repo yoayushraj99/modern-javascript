57db5ab29276b4f84cf64855b610e2e2936a29f4
yoayushraj99 <56199773+yoayushraj99@users.noreply.github.com>
2020-04-20 00:31:54 +0530

    Update sandbox.js

--- chapter_15/sandbox.js
+++ chapter_15/sandbox.js
@@ -1,3 +1,8 @@
+/*Unlike regular functions, arrow functions do not have their own this. The value of this inside an arrow function 
+remains the same throughout the lifecycle of the function and is always bound to the value of this in the closest 
+non-arrow parent function.*/
+
+
 class User {
   constructor(username, email){
     this.username = username;
