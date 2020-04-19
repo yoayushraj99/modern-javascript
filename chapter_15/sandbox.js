3ce5dcb0777c64389085eff8a084a739c085ba66
yoayushraj99 <56199773+yoayushraj99@users.noreply.github.com>
2020-04-20 00:33:13 +0530

    Update sandbox.js

--- chapter_15/sandbox.js
+++ chapter_15/sandbox.js
@@ -1,6 +1,11 @@
 /*Unlike regular functions, arrow functions do not have their own this. The value of this inside an arrow function 
 remains the same throughout the lifecycle of the function and is always bound to the value of this in the closest 
-non-arrow parent function.*/
+non-arrow parent function.
+Regular functions created using function declarations or expressions are constructible and callable. 
+Since regular functions are constructible, they can be called using the new keyword.
+However, the arrow functions are only callable and not constructible, i.e arrow functions can never be used as constructor functions.
+Hence, they can never be invoked with the new keyword.
+*/
 
 
 class User {
